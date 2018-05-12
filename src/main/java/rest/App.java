392eb0a3164950ae255986671f754f768210fa57
Mine <wokadakow@users.noreply.github.com>
2018-05-12 18:25:32 +0200

    Refactor

--- src/main/java/rest/App.java
+++ src/main/java/rest/App.java
@@ -1,16 +1,15 @@
 package rest;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
 
 
         System.out.println("Next line");
+    }
+
+
+    public static Integer doubleIt(Integer a){
+        return a * 2;
     }
 }
