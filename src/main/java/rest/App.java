8ade0ea7920c08479544594c1e47afa8ba2b15da
Mine <wokadakow@users.noreply.github.com>
2018-05-12 18:21:37 +0200

    Hello world

---
+++ src/main/java/rest/App.java
@@ -0,0 +1,16 @@
+package rest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+
+        System.out.println("Next line");
+    }
+}
